795cb39fd7b54a631042f00d604b56f4399c49ee
Alex <Trebek@users.noreply.github.com>
2013-11-07 03:51:35 -0800

    Last commit was a lie.
    NOW it works properly. Boy, am I dumb.

--- pyfudge.py
+++ pyfudge.py
@@ -1,8 +1,8 @@
 #===============================================================================
 # PyFudge: A Python-based Brainfuck Interpreter
 #-------------------------------------------------------------------------------
-# Version: 0.1.2
-# Updated: 06-11-2013
+# Version: 0.1.3
+# Updated: 07-11-2013
 # Author: Alex Crawford
 # License: MIT
 # Demo: http://repl.it/MOC
@@ -10,8 +10,7 @@
 
 '''PyFudge is a Python-based (2.7.5) Brainfuck interpreter.
 
-I believe it's fully functional (nested loops and all), but haven't
-tested it thoroughly yet.
+I believe it's fully functional, but haven't tested it thoroughly yet.
 
 '''
 
@@ -34,8 +33,8 @@ MEMORY = [0] * MEMSIZE
 # PYFUDGE MAIN FUNCTION
 #===============================================================================
 
-def read(program, wrap=True):
-    '''pyfudge.read(program, wrap=True)
+def run(program, wrap=True):
+    '''pyfudge.run(program, wrap=True)
 
     Interprets a Brainfuck program fed into it.
 
@@ -44,52 +43,54 @@ def read(program, wrap=True):
     pointer = 0
     progpos = 0
 
-    loopstack = []
-
     program = program.replace(" ", "")
 
-    while True:
+    outbuff = []
 
-        try:
+    while progpos < len(program):
 
-            for char in program[progpos]:
+        char = program[progpos]
 
-                if char is '+':
-                    MEMORY[pointer] += 1
-                elif char is '-':
-                    MEMORY[pointer] -= 1
-                elif char is '>':
-                    pointer += 1
-                elif char is '<':
-                    pointer -= 1
-                elif char is '.':
-                    if MEMORY[pointer] != 0:
-                        print(chr(MEMORY[pointer]), end="")
-                elif char is ',':
-                    MEMORY[pointer] = ord(raw_input())
-                elif char is '[':
-                    if MEMORY[pointer] == 0:
-                        while True:
-                            if program[progpos] is not ']':
-                                progpos += 1
-                            else:
-                                break
-                    loopstack.append(progpos - 1)
-                elif char is ']':
-                    if MEMORY[pointer] != 0:
-                        progpos = loopstack[-1]
-                    loopstack.pop(-1)
-                elif char  is '#':
-                    print(MEMORY[pointer])
-
-                if wrap:
-                    if MEMORY[pointer] > BYTESIZE:
-                        MEMORY[pointer] = 0
-                    elif MEMORY[pointer] < 0:
-                        MEMORY[pointer] = BYTESIZE
-
+        if char is '+':
+            MEMORY[pointer] += 1
+        elif char is '-':
+            MEMORY[pointer] -= 1
+        elif char is '>':
+            pointer += 1
+        elif char is '<':
+            pointer -= 1
+        elif char is '.':
+            if MEMORY[pointer] == 10:
+                outbuff.extend('\n')
+            else:
+                outbuff.extend(chr(MEMORY[pointer]))
+        elif char is ',':
+            MEMORY[pointer] = ord(raw_input())
+        elif char is '[' and MEMORY[pointer] == 0:
+            loops = 1
+            while loops > 0:
                 progpos += 1
+                if program[progpos] is '[':
+                    loops += 1
+                elif program[progpos] is ']':
+                    loops -= 1
+        elif char is ']' and MEMORY[pointer] != 0:
+            loops = 1
+            while loops > 0:
+                progpos -= 1
+                if program[progpos] is '[':
+                    loops -= 1
+                elif program[progpos] is ']':
+                    loops += 1
+        elif char is '#':
+            print(MEMORY[pointer])
 
-        except:
+        if wrap:
+            if MEMORY[pointer] > BYTESIZE:
+                MEMORY[pointer] = 0
+            elif MEMORY[pointer] < 0:
+                MEMORY[pointer] = BYTESIZE
 
-            break
+        progpos += 1
+
+    return "".join(outbuff)
