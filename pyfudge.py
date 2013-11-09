f0262ec640bce5786dfa7f3ae937cf9798c712df
Alex <Trebek@users.noreply.github.com>
2013-11-08 18:27:04 -0800

    Now uses a list for keeping track of loop jump points.

--- pyfudge.py
+++ pyfudge.py
@@ -1,10 +1,11 @@
 #===============================================================================
 # PyFudge: A Python-based Brainfuck Interpreter
 #-------------------------------------------------------------------------------
-# Version: 0.1.3
-# Updated: 07-11-2013
+# Version: 0.1.4
+# Updated: 08-11-2013
 # Author: Alex Crawford
 # License: MIT
+# Demo: http://repl.it/MQz/1
 #===============================================================================
 
 '''PyFudge is a Python-based (2.7.5) Brainfuck interpreter.
@@ -23,73 +24,126 @@ from __future__ import print_function
 # MEMORY VARIABLES
 #===============================================================================
 
-MEMSIZE = 30000
-BYTESIZE = 255
-
-MEMORY = [0] * MEMSIZE
+MEMSIZE = 'Auto'
+CELLSIZE = 255
+MEMORY = [0]
 
 #===============================================================================
-# PYFUDGE MAIN FUNCTION
+# PYFUDGE FUNCTIONS
 #===============================================================================
 
-def run(program, wrap=True):
-    '''pyfudge.run(program, wrap=True)
+def run(program, wrap=True, binary=False, debug=False):
+    '''pyfudge.run(program, wrap=True, binary=False, debug=False)
 
     Interprets a Brainfuck program fed into it.
 
     '''
 
-    pointer = 0
+    mempos = 0
     progpos = 0
-
-    program = program.replace(" ", "")
 
     outbuff = []
 
-    while progpos < len(program):
+    program = program.replace(" ", "")
+    proglen = len(program)
+
+    looplst = findloops(program, proglen)
+    looplen = program.count('[')
+
+    if 'ERROR' in looplst[0]:
+        return looplst
+
+    while progpos < proglen:
 
         char = program[progpos]
 
         if char is '+':
-            MEMORY[pointer] += 1
+            MEMORY[mempos] += 1
         elif char is '-':
-            MEMORY[pointer] -= 1
+            MEMORY[mempos] -= 1
         elif char is '>':
-            pointer += 1
+            mempos += 1
         elif char is '<':
-            pointer -= 1
+            mempos -= 1
         elif char is '.':
-            if MEMORY[pointer] == 10:
+            if not binary and MEMORY[mempos] == 10:
                 outbuff.extend('\n')
             else:
-                outbuff.extend(chr(MEMORY[pointer]))
+                outbuff.extend(chr(MEMORY[mempos]))
         elif char is ',':
-            MEMORY[pointer] = ord(raw_input())
-        elif char is '[' and MEMORY[pointer] == 0:
-            loops = 1
-            while loops > 0:
-                progpos += 1
-                if program[progpos] is '[':
-                    loops += 1
-                elif program[progpos] is ']':
-                    loops -= 1
-        elif char is ']' and MEMORY[pointer] != 0:
-            loops = 1
-            while loops > 0:
-                progpos -= 1
-                if program[progpos] is '[':
-                    loops -= 1
-                elif program[progpos] is ']':
-                    loops += 1
-        elif char is '#':
-            print(MEMORY[pointer])
+            cmd = raw_input()
+            if cmd is not '':
+                MEMORY[mempos] = ord(cmd)
+            else:
+                MEMORY[mempos] = 0
+        elif char is '[':
+            if MEMORY[mempos] == 0:
+                for i in range(looplen):
+                    if progpos == looplst[i][0]:
+                        progpos = looplst[i][1]
+        elif char is ']':
+            if MEMORY[mempos] != 0:
+                for i in range(looplen):
+                    if progpos == looplst[i][1]:
+                        progpos = looplst[i][0]
+        elif char is '#' and debug:
+            print(MEMORY[mempos])
+
+        if MEMSIZE is 'Auto':
+            try:
+                test = MEMORY[mempos]
+            except:
+                MEMORY.extend([0])
+        else:
+            try:
+                test = MEMORY[mempos]
+            except:
+                return ['ERROR: Not enough memory.']
 
         if wrap:
-            if MEMORY[pointer] > BYTESIZE:
-                MEMORY[pointer] = 0
-            elif MEMORY[pointer] < 0:
-                MEMORY[pointer] = BYTESIZE
+            if MEMORY[mempos] > CELLSIZE:
+                MEMORY[mempos] = 0
+            elif MEMORY[mempos] < 0:
+                MEMORY[mempos] = CELLSIZE
 
         progpos += 1
 
-    return "".join(outbuff)
+    return "".join(outbuff), MEMORY 
+
+def findloops(program, proglen):
+    '''findloops(program, proglen) -> list of loop jump points'''
+
+    temp = []
+    looplst = []
+
+    if program.count('[') == program.count(']'):
+        for i in range(proglen):
+            if program[i] is '[':
+                temp.append(i)
+            elif program[i] is ']':
+                index = temp.pop()
+                looplst.append([index, i])
+        return looplst
+    else:
+        return ['ERROR: Mismatched number of loop brackets.']
+
+def setmemory(memsize='Auto', cellsize=None):
+    '''setmemory(memsize='Auto', cellsize=None)
+
+    Sets the memory, and cell size to the specified sizes.
+
+    '''
+
+    global MEMSIZE
+    global CELLSIZE
+    global MEMORY
+
+    if memsize is 'Auto':
+        MEMSIZE = 'Auto'
+    else:
+        MEMSIZE = memsize
+        MEMORY = [0] * MEMSIZE
+        
+    if cellsize is not None:
+        CELLSIZE = cellsize
+
